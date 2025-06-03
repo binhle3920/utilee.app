0a60af14d0fc6150054a4da86f382b75841165c3
binhle3920 <lethanhbinh3920@gmail.com>
2025-06-03 22:39:40 +0700

    feat: add buy me a coffee button

--- src/containers/dashboard/layout/index.tsx
+++ src/containers/dashboard/layout/index.tsx
@@ -1,9 +1,13 @@
-import { Layout, Typography } from 'antd';
+import { Flex, Layout, Typography } from 'antd';
 import { Outlet } from 'react-router-dom';
 
 import Container from '../../../components/container';
 
 const DashboardLayout = () => {
+  const onBuyMeACoffee = () => {
+    window.open('https://coff.ee/binhle', '_blank');
+  };
+
   return (
     <Layout className='h-screen'>
       <Layout.Content className='p-0 w-full bg-white!'>
@@ -13,7 +17,17 @@ const DashboardLayout = () => {
       </Layout.Content>
       <Layout.Footer className='p-0'>
         <Container>
-          <Typography.Text className='text-center w-full'>©2025 Created by DEV Utilities</Typography.Text>
+          <Flex justify='between' align='center'>
+            <Typography.Text className='w-full'>©2025 Created by Binh Le</Typography.Text>
+
+            {/* Generate a buy me a coffee button */}
+            <img
+              src={'https://cdn.buymeacoffee.com/buttons/default-orange.png'}
+              alt='Buy me a coffee'
+              className='w-40 aspect-2.8/1 rounded-lg shadow-md cursor-pointer hover:scale-105 hover:shadow-lg transition-all duration-300'
+              onClick={onBuyMeACoffee}
+            />
+          </Flex>
         </Container>
       </Layout.Footer>
     </Layout>
